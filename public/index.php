699732c2a6a332027a733eb5f5a50069556839c1
Joey Smith <jsmith@webinertia.net>
2025-04-21 17:51:41 -0500

    Cleanup Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-// Delegate static file requests back to the PHP built-in webserver
+// Back to the PHP built-in webserver
 if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__) {
     return false;
 }
@@ -10,14 +10,12 @@ if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__) {
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
 
-/**
- * Self-called anonymous function that creates its own scope and keeps the global namespace clean.
- */
+
 (function () {
     /** @var \Psr\Container\ContainerInterface $container */
     $container = require 'config/container.php';
 
-    /** @var \Mezzio\Application $app */
+    /** @var \Example\App $app */
     $app = $container->get(\Example\App::class);
 
     $app->run();
