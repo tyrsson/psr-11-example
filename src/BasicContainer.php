64e66cd84d7ea01fc04057d68f973a7b92f4e51a
Joey Smith <jsmith@webinertia.net>
2025-04-21 19:35:42 -0500

    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- src/BasicContainer.php
+++ src/BasicContainer.php
@@ -7,6 +7,7 @@ namespace Example;
 use Example\Exception\ServiceNotCreatedException;
 use Example\Exception\ServiceNotFoundException;
 use Psr\Container\ContainerInterface;
+use Override;
 
 use function class_exists;
 use function is_callable;
@@ -28,6 +29,25 @@ class BasicContainer implements ContainerInterface
         $this->configure($config);
     }
 
+    #[Override]
+    public function get(string $id): array|object
+    {
+        if (isset($this->services[$id])) {
+            return $this->services[$id];
+        }
+
+        return $this->createService($id);
+    }
+
+    #[Override]
+    public function has(string $id): bool
+    {
+        if (isset($this->services[$id]) || isset($this->factories[$id])) {
+            return true;
+        }
+        return false;
+    }
+
     public function configure(array $config): self
     {
         if (isset($config['services'])) {
@@ -41,18 +61,22 @@ class BasicContainer implements ContainerInterface
         return $this;
     }
 
-    public function set(string $id, object $service): void
+    private function createService(string $id, ?array $options = null): object
     {
-        $this->services[$id] = $service;
-    }
-
-    public function get(string $id): array|object
-    {
-        if (isset($this->services[$id])) {
-            return $this->services[$id];
+        try {
+            $factory = $this->getFactory($id);
+            $instance = $factory($this->container, $id, $options);
+        } catch (ServiceNotFoundException $e) {
+            throw $e;
+        } catch (\Exception $e) {
+            throw new ServiceNotCreatedException(sprintf(
+                'Service with id "%s" could not be created. Reason: %s',
+                $id,
+                $e->getMessage()
+            ), (int) $e->getCode(), $e);
         }
 
-        return $this->createService($id);
+        return $factory($this);
     }
 
     private function getFactory(string $id): callable
@@ -72,31 +96,5 @@ class BasicContainer implements ContainerInterface
         }
 
         throw new ServiceNotFoundException(sprintf('Service "%s" not found.', $id));
-    }
-
-    public function has(string $id): bool
-    {
-        if (isset($this->services[$id]) || isset($this->factories[$id])) {
-            return true;
-        }
-        return false;
-    }
-
-    private function createService(string $id, ?array $options = null): object
-    {
-        try {
-            $factory = $this->getFactory($id);
-            $instance = $factory($this->container, $id, $options);
-        } catch (ServiceNotFoundException $e) {
-            throw $e;
-        } catch (\Exception $e) {
-            throw new ServiceNotCreatedException(sprintf(
-                'Service with id "%s" could not be created. Reason: %s',
-                $id,
-                $e->getMessage()
-            ), (int) $e->getCode(), $e);
-        }
-
-        return $factory($this);
     }
 }
